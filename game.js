1910caad0134fab47ab5dcc9339f292070e18c1c
Arpita Chikkodi <apschikkodi14@gmail.com>
2020-07-29 20:01:24 +0530

    End Page

--- game.js
+++ game.js
@@ -59,6 +59,8 @@ startGame = () => {
 getNewQuestion = () => {
 
     if(availableQuestions.length===0 || questionCounter >= MAX_QUESTIONS) {
+
+        localStorage.setItem("mostRecentScore", score);
         // goto the end page
         return window.location.assign("/end.html");
     }
