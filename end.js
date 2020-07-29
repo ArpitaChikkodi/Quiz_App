d752cbc34b15fa7023d2546c3f225a819dd45cb4
Arpita Chikkodi <apschikkodi14@gmail.com>
2020-07-29 22:56:02 +0530

    Storing High Score locally

--- end.js
+++ end.js
@@ -3,6 +3,11 @@ const saveScoreBtn = document.getElementById('saveScoreBtn');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 const finalScore = document.getElementById('finalScore');
 
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const MAX_HIGH_SCORES = 5;
+// console.log(highScores);
+
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', () => {
@@ -11,6 +16,26 @@ username.addEventListener('keyup', () => {
 })
 
 saveHighScore = e => {
-    console.log("Clicked the save button!");
+    // console.log("Clicked the save button!");
     e.preventDefault();
-}
+
+    const score = {
+        // checking
+        // score: Math.floor(Math.random() * 100),
+        score: mostRecentScore,
+        name: username.value
+    };
+    highScores.push(score);
+
+    // need only top 5 highscores
+    highScores.sort( (a,b) =>  b.score - a.score);
+        // if b score is greater than a score put b first so sorts in decreasing order
+    
+    highScores.splice(5);
+
+    // note it stores in string format so JSON is stringyfied
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    // go to index.html or home page
+    window.location.assign('/');
+    // console.log(highScores);
+};
