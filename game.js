4e4f700b96831edbb4981599db5ec54919a16810
Arpita Chikkodi <apschikkodi14@gmail.com>
2020-07-30 18:33:18 +0530

    Loading Questions from json file

--- game.js
+++ game.js
@@ -15,33 +15,21 @@ score = 0;
 questionCounter = 0;
 availableQuestions = [];
 
-let questions = [
-    {
-        question: "Inside which HTML element do we put Javascript code in?",
-        choice1: '<script>',
-        choice2: '<javascript>',
-        choice3: '<js>',
-        choice4: '<scripting>',
-        answer: 1,
+let questions = [];
 
-    },
-    {
-        question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
-        choice1: "<script href='xxx.js'>",
-        choice2: "<script name='xxx.js'>",
-        choice3: "<script src='xxx.js'>",
-        choice4: "<script file='xxx.js'>",
-        answer: 3,
-    },
-    {
-        question: " How do you write 'Hello World' in an alert box?",
-        choice1: "msgBox('Hello World');",
-        choice2: "alertBox('Hello World');",
-        choice3: "msg('Hello World');",
-        choice4: "alert('Hello World');",
-        answer: 4,
-    },
-];
+fetch("questions.json")
+.then(res => {
+    // console.log(res);
+    return res.json();
+}).then( loadedQuestions => {
+    console.log(loadedQuestions);
+
+    questions = loadedQuestions;
+    startGame();
+})
+.catch( err => {
+    console.error(err);
+})
 
 // CONSTANTS
 const CORRECT_BONUS = 10;
@@ -123,5 +111,3 @@ incrementscore = num => {
     scoreText.innerText = score;
 
 };
-
-startGame();
